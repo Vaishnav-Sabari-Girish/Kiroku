5ecdb8728204538bf59ea4fd84bc75a3639c8675
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-08-19 11:10:59 +0530

    Added basic User input

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,18 @@ mod truth_table;
 
 use parser::parse_expr;
 use truth_table::truth_table;
+use std::io;
 
 fn main() {
-    let input = "((A * B) | (!C * !(A + B)))";
+    println!("Enter the Expression : ");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read Input");
     println!();
     println!("Expression: {}", input);
     println!();
 
-    let expr = parse_expr(input);
+    let expr = parse_expr(input.as_str());
     truth_table(&expr);
 }
